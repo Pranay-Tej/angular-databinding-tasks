df43b393e331334f7b0e951876635b4441991d92
Pranay-Tej <s.pranayteja@gmail.com>
2019-10-29 09:38:47 +0530

    task1

--- angular-task1/src/app/component1/component1.component.ts
+++ angular-task1/src/app/component1/component1.component.ts
@@ -1,18 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Input } from '@angular/core';
-
 @Component({
-  selector: 'app-component-1',
-  templateUrl: './component-1.component.html',
-  styleUrls: ['./component-1.component.css']
+  selector: 'app-component1',
+  templateUrl: './component1.component.html',
+  styleUrls: ['./component1.component.css']
 })
 export class Component1Component implements OnInit {
   displayInConsole(userInput){
     console.log(userInput);
   }
-  @Input() name: string;
-
   constructor() { }
 
   ngOnInit() {
